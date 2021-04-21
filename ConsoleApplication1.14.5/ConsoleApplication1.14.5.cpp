aa2c6f153f2bf45488d7a310f2014372e254d79c
Alexandr <VeselovskiyAlexandr@mail.ru>
2021-04-21 15:15:48 +0300

    test 5

--- ConsoleApplication1.14.5/ConsoleApplication1.14.5.cpp
+++ ConsoleApplication1.14.5/ConsoleApplication1.14.5.cpp
@@ -3,14 +3,78 @@
 
 #include <iostream>
 #include <locale.h>
+#include <string>
 using namespace std;
 
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-   
-}
+    int i, counter=0;
+	string str, str0, str1, str2, str3;
+	char ch;
+	bool correct=true;
+	cout << "\nВведите IP-адрес ";
+	getline(cin, str);
+	for (i = 0; i < str.length(); i++) {
+
+		if (str[i] == ' ') {
+			cout << "\n No"; break; correct = false;
+		}
+		else if ((str[0] == '0') && (str[1] == '0')) {
+			cout << "\n No"; break; correct = false;
+		}
+		else if ((str[i] == '.') && (str[i + 1] == '.')) {
+			cout << "\n No"; break; correct = false;
+		}
+		else if ((str[i] == '.') && (str[i + 1] == '0') && (str[i + 2] == '0')) {
+			cout << "\n No"; break; correct = false;
+		}
+		else if (((str[i] < '0') || (str[i] > '9')) && (str[i] != '.')) {
+			cout << "\n No"; break; correct = false;
+		}
+		else if ((str[i] != '.') && (counter == 0)) {
+			ch = str[i]; str0 += ch;
+		}
+		else if ((str[i] != '.') && (counter == 1)) {
+			ch = str[i]; str1 += ch;
+		}
+		else if ((str[i] != '.') && (counter == 2)) {
+			ch = str[i]; str2 += ch;
+		}
+		else if ((str[i] != '.') && (counter == 3)) {
+			ch = str[i]; str3 += ch;
+		}
+		else if (str[i] == '.') {
+			counter++;
+		}
+		else if (counter > 3) {
+			cout << "\n No"; break; correct = false;
+		}
+	}
+		 if ((str0.length()>3)||(str1.length()>3)||(str2.length()>3)||(str3.length()>3)) {
+				cout << "\n No"; correct = false;
+			}
+		else if((str0.length()== 3)&&((100*(str0[0]-'0')+10*(str0[1]-'0')+(str0[2]-'0'))>255)){
+			cout << "\n No"; correct = false;
+			}
+		else if ((str1.length() == 3)&&((100*(str1[0] - '0')+10 *(str1[1] - '0')+(str1[2] - '0'))>255)) {
+			cout << "\n No"; correct = false;
+		}
+		else if ((str2.length() == 3)&&((100 * (str2[0] -'0')+10*(str2[1]-'0')+(str2[2]-'0')) > 255)) {
+			cout << "\n No"; correct = false;
+		}
+		else if ((str3.length() == 3) && ((100 * (str3[0] - '0') + 10 * (str3[1] - '0') + (str3[2] - '0')) > 255)) {
+			cout << "\n No"; correct = false;
+		}
+		else if(correct==true) {
+			cout << "\n Yes";
+		}
+		}
+			
+		
+	
+
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
 // Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
